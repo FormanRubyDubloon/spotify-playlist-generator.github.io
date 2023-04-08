38138d4a1e55e6b215c4d923d3c56fefa214af1a
Isaac Forman <isaac@Polygon.local>
2023-04-08 12:01:59 +0900

    Initial implementation of the playlist generator

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ function parseCSV(file) {
   });
 }
 
+async function searchTrack(accessToken, artist, title) {
+  const query = encodeURIComponent(`artist:${artist} track:${title}`);
+  const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=track&limit=1`, {
+    headers: { 'Authorization': `Bearer ${accessToken}` },
+  });
+
+  const data = await response.json();
+  if (data.tracks.items.length > 0) {
+    return data.tracks.items[0].id;
+  }
+  return null;
+}
+
 async function createPlaylist(accessToken, userId, tracks) {
   const apiBaseUrl = 'https://api.spotify.com/v1';
   const headers = {
@@ -33,7 +46,7 @@ async function createPlaylist(accessToken, userId, tracks) {
   });
 
   const playlist = await playlistResponse.json();
-  const uris = tracks.map(track => `spotify:track:${track.spotify_id}`);
+  const uris = tracks.map(track => `spotify:track:${track}`);
 
   await fetch(`${apiBaseUrl}/playlists/${playlist.id}/tracks`, {
     method: 'POST',
@@ -52,7 +65,6 @@ async function createPlaylist(accessToken, userId, tracks) {
   const accessToken = params.get('access_token');
   if (!accessToken) return;
 
-  // Hide the "Authenticate" button and show the "File Selection" after authentication
   document.getElementById('authenticate').style.display = 'none';
   document.getElementById('fileSelection').style.display = 'block';
 
@@ -61,14 +73,22 @@ async function createPlaylist(accessToken, userId, tracks) {
     const file = fileInput.files[0];
     if (!file) return;
 
-    const tracks = await parseCSV(file);
+    const trackList = await parseCSV(file);
+    const trackIds = [];
+
+    for (const track of trackList) {
+      const trackId = await searchTrack(accessToken, track.artist, track.title);
+      if (trackId) {
+        trackIds.push(trackId);
+      }
+    }
+
     const userProfileResponse = await fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': `Bearer ${accessToken}` },
     });
 
     const userProfile = await userProfileResponse.json();
-    const playlistUrl = await createPlaylist(accessToken, userProfile.id, tracks);
+    const playlistUrl = await createPlaylist(accessToken, userProfile.id, trackIds);
     window.location.href = playlistUrl;
   });
 })();
-
