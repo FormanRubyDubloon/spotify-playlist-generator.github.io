a81e49284412d797b1ad58e090eddd6f3dbf92bd
Isaac Forman <isaac@Polygon.local>
2023-04-08 18:25:24 +0900

    Initial implementation of the playlist generator

--- app.js
+++ app.js
@@ -9,15 +9,25 @@ function onAuthenticateButtonClick() {
 
 document.getElementById('authenticate').addEventListener('click', onAuthenticateButtonClick);
 
-function parseCSV(file) {
-  return new Promise((resolve) => {
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        resolve(results.data);
-      },
-    });
+
+async function fetchGptSuggestions(prompt) {
+  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer 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',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 100,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    }),
   });
+
+  const data = await response.json();
+  return data.choices[0].text;
 }
 
 async function searchTrack(accessToken, artist, title) {
@@ -83,32 +93,37 @@ async function createTextPlaylist(accessToken, trackList, trackIds) {
   document.getElementById('authenticate').style.display = 'none';
   document.getElementById('fileSelection').style.display = 'block';
 
-  document.getElementById('generatePlaylist').addEventListener('click', async () => {
-    const fileInput = document.getElementById('csvFile');
-    const file = fileInput.files[0];
-    if (!file) return;
+document.getElementById('generatePlaylist').addEventListener('click', async () => {
+  const chatInput = document.getElementById('gptChatInput').value;
+  if (!chatInput) return;
 
-    const trackList = await parseCSV(file);
-    const trackIds = [];
+  const gptSuggestions = await fetchGptSuggestions(chatInput);
+  const lines = gptSuggestions.split('\n').filter(line => line.trim());
 
-    for (const track of trackList) {
-      const trackId = await searchTrack(accessToken, track.artist, track.title);
-      if (trackId) {
-        trackIds.push(trackId);
-      }
-    }
-
-    const userProfileResponse = await fetch('https://api.spotify.com/v1/me', {
-      headers: { 'Authorization': `Bearer ${accessToken}` },
-    });
-
-    const userProfile = await userProfileResponse.json();
-    const playlistUrl = await createPlaylist(accessToken, userProfile.id, trackIds);
-
-    // Call the createTextPlaylist function to render the text playlist
-    await createTextPlaylist(accessToken, trackList, trackIds);
-
-    // Open the generated playlist in a new window
-    window.open(playlistUrl, '_blank');
+  const trackList = lines.map(line => {
+    const [artist, title] = line.split('-').map(s => s.trim());
+    return { artist, title };
   });
+
+  const trackIds = [];
+
+  for (const track of trackList) {
+    const trackId = await searchTrack(accessToken, track.artist, track.title);
+    if (trackId) {
+      trackIds.push(trackId);
+    }
+  }
+
+  const userProfileResponse = await fetch('https://api.spotify.com/v1/me', {
+    headers: { 'Authorization': `Bearer ${accessToken}` },
+  });
+
+  const userProfile = await userProfileResponse.json();
+  const playlistUrl = await createPlaylist(accessToken, userProfile.id, trackIds);
+
+  await createTextPlaylist(accessToken, trackList, trackIds);
+
+  window.open(playlistUrl, '_blank');
+});
+
 })();
